d20beccb8c8612d82c5f5ea74359b8c5bd76eccc
YannickJacquesARTM <yjacques@artm.quebec>
2025-10-27 13:51:23 -0400

    Lower minimum iOS version to 14 in Package.swift
    Updated the Swift package manifest to support iOS 14 instead of iOS 15, allowing the library to be used in projects targeting iOS 14 and above.

--- Package.swift
+++ Package.swift
@@ -5,7 +5,7 @@ import PackageDescription
 
 let package = Package(
     name: "NGLoggerKit",
-    platforms: [.iOS(.v15)],
+    platforms: [.iOS(.v14)],
     products: [
         .library(
             name: "NGLoggerKit",
